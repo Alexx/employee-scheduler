f799ca8a2f2611fa2e4b9c1602bc1e90ffab76a7
Alex Siegberg <alex.siegberg@gmail.com>
2019-09-20 13:48:03 -0700

    Generate scheduled employees dynimally.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Row, Col, Container } from "react-bootstrap";
 import Navibar from "./components/Navibar";
 import AddEmployeeForm from "./components/AddEmployeeForm";
 import SignInSignUp from "./components/SignInSignUp";
+import Schedule from "./components/Schedule";
 
 function App() {
   return (
     <div className="App">
       <Navibar />
-      <Container>
-        <SignInSignUp />
-      </Container>
+
+      <Schedule />
     </div>
   );
 }
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,43 +1,85 @@
 import React from "react";
 import { Card, ListGroup } from "react-bootstrap";
 import { v4 } from "uuid";
+import Moment from "moment";
 
 const initialState = {
-  scheduledEmployees: {
-    [v4()]: {
-      firstName: "Alex",
-      lastName: "Siegberg",
-      monAvaFrom: 800,
-      monAvaTo: 2400,
-      tueAvaFrom: 800,
-      tueAvaTo: 2400,
-      wedAvaFrom: 800,
-      wedAvaTo: 2400,
-      thuAvaFrom: 800,
-      thuAvaTo: 2400,
-      friAvaFrom: 800,
-      friAvaTo: 2400,
-      satAvaFrom: 800,
-      satAvaTo: 2400,
-      sunAvaFrom: 800,
-      sunAvaTo: 2400
+  employees: {
+    monEmployees: {
+      [v4()]: {
+        firstName: "Alex",
+        lastName: "Siegberg",
+        startTime: "08:00:00",
+        endTime: "16:00:00"
+      },
+      [v4()]: {
+        firstName: "Bob",
+        lastName: "Burger",
+        startTime: "09:00:00",
+        endTime: "17:00:00"
+      }
+    },
+    tueEmployees: {
+      [v4()]: {
+        firstName: "Alex",
+        lastName: "Siegberg",
+        startTime: "08:00:00",
+        endTime: "16:00:00"
+      },
+      [v4()]: {
+        firstName: "Bob",
+        lastName: "Burger",
+        startTime: "09:00:00",
+        endTime: "17:00:00"
+      }
     }
   }
 };
 
-let employeeList = [];
-for (let id in initialState) {
-  employeeList.push(
-    <ListGroup.Item></ListGroup.Item>
-  )
+const convert = time => {
+  return Moment(time, "HH:mm:ss").format("h:mmA");
+};
+
+let monEmployeeList = [];
+let tueEmployeeList = [];
+for (let id in initialState.employees.monEmployees) {
+  monEmployeeList.push(
+    <ListGroup.Item>
+      {`
+      ${convert(initialState.employees.monEmployees[id].startTime)} -
+      ${convert(initialState.employees.monEmployees[id].endTime)}
+      ${initialState.employees.monEmployees[id].firstName}
+      ${initialState.employees.monEmployees[id].lastName}
+      `}
+    </ListGroup.Item>
+  );
+}
+
+for (let id in initialState.employees.tueEmployees) {
+  tueEmployeeList.push(
+    <ListGroup.Item>
+      {`
+      ${convert(initialState.employees.tueEmployees[id].startTime)} -
+      ${convert(initialState.employees.tueEmployees[id].endTime)}
+      ${initialState.employees.tueEmployees[id].firstName}
+      ${initialState.employees.tueEmployees[id].lastName}
+      `}
+    </ListGroup.Item>
+  );
 }
 
 function Schedule() {
   return (
     <div>
-      <Card>
-        <Card.Header>Monday</Card.Header>
-        <ListGroup variant="flush">{employeeList}</ListGroup>
+      <Card style={{ width: "14%" }}>
+        <ListGroup variant="flush">
+          <ListGroup.Item>Openers</ListGroup.Item>
+          {monEmployeeList}
+        </ListGroup>
+      </Card>
+      <Card style={{ width: "14%" }}>
+        <ListGroup.Item>Openers</ListGroup.Item>
+        <ListGroup variant="flush">{tueEmployeeList}</ListGroup>
       </Card>
     </div>
   );
